eaf59356fe4274938dedef2b769d126e05a12e2a
kenta <nakamori.k@gmail.com>
2021-05-06 14:03:04 +0900

    delete pycahe

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,9 +1,16 @@
 from flaskapp import app, db
 from flask import render_template, redirect, url_for, flash, request, jsonify
 from flask_login import login_user, current_user, logout_user, login_required
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
         if not current_user.is_authenticated:
             return redirect(url_for('register'))
         return render_template("home.html", title="Instaclone")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = 
